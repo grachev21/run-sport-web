96dbda6895f0a3b01e6c9c285e37d0253401b814
grachev <grachev@grachev.grachev>
2023-12-19 13:15:10 +0400

    01 Пустой проект

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Начало</h1>
+      <p>hello world</p>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+<App />, 
+// Загружает из public/index.html
+document.getElementById("root")
+);
