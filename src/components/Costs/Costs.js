99d3be013c2de21a7b89526c48e5af52b7a1264d
grachev <grachev@grachev.grachev>
2023-12-22 14:49:44 +0400

    13 Отображаем наш массив как цикл в map()

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -22,23 +22,16 @@ const Costs = (props) => {
     <Card className='costs'>
     <CostsFilter 
         year={selectedYear} 
-        onChangeYear={yearChangeHandler}
-      />
-    <CostItem
-        date={props.costs[0].date}
-        description={props.costs[0].description}
-        amount={props.costs[0].amount}
-    />
-    <CostItem
-        date={props.costs[1].date}
-        description={props.costs[1].description}
-        amount={props.costs[1].amount}
-    />
-    <CostItem
-        date={props.costs[2].date}
-        description={props.costs[2].description}
-        amount={props.costs[2].amount}
-    />
+        onChangeYear={yearChangeHandler}/>
+
+      {/* Это работает так же как цикл */}
+      {props.costs.map((cost) => (
+        <CostItem 
+        date={cost.date}
+        description={cost.description}
+        amount={cost.amount}/>
+      ))}
+
     </Card>
   </div>
   );
